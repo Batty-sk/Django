d46662b4451ab99d87a14497f08164c233242b8e
Saurav kumar <saorav.skumar@gmail.com>
2023-12-06 16:42:17 +0530

    Delete ChatApplication directory

--- ChatApplication/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User_pics
-# Register your models here.
-
-class Show_user_pics(admin.ModelAdmin):
-    list_display=['user','pic']
-
-admin.site.register(User_pics,Show_user_pics)
